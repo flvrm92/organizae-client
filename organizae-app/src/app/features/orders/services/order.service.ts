061728b8c544cde4db76f869a6e14bb2a1aba7e4
Flavio Moreno <flavioricardo3000@hotmail.com>
2026-04-19 16:40:01 -0300

    feat: enhance order list with advanced filtering options and update API service for parameterized requests

--- organizae-app/src/app/features/orders/services/order.service.ts
+++ organizae-app/src/app/features/orders/services/order.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, inject } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../../core/services/api.service';
 import { IOrder } from '../../../../types/IOrder';
@@ -8,8 +9,19 @@ import { IOrderHistory } from '../../../../types/IOrderHistory';
 export class OrderService {
   private readonly api = inject(ApiService);
 
-  getAll(): Observable<IOrder[]> {
-    return this.api.get<IOrder[]>('/api/Order');
+  getAll(filters?: { customerId?: string; startDate?: string; endDate?: string; statusIds?: string[] }): Observable<IOrder[]> {
+    if (!filters) return this.api.get<IOrder[]>('/api/Order');
+
+    let params = new HttpParams();
+    if (filters.customerId) params = params.set('customerId', filters.customerId);
+    if (filters.startDate) params = params.set('startDate', filters.startDate);
+    if (filters.endDate) params = params.set('endDate', filters.endDate);
+    if (filters.statusIds?.length) {
+      for (const id of filters.statusIds) {
+        params = params.append('statusIds', id);
+      }
+    }
+    return this.api.getWithParams<IOrder[]>('/api/Order', params);
   }
 
   getById(id: string): Observable<IOrder> {
